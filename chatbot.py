793fd6691a04aa9674e9c0a6f3520052e73fd564
Bhargav Thanki <bhargavthanky@gmail.com>
2017-10-27 16:38:46 -0400

    Removed db connection

--- chatbot.py
+++ chatbot.py
@@ -3,8 +3,8 @@ from models.database import db
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 
-db.init_app(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 
+#db.init_app(app)
 
 @app.route('/')
 def hello_world():
