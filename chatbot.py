3573c0f6d2faaa8389eacc4364d3b17f93c054f6
bmthanki <bhargavthanky@gmail.com>
2017-10-28 01:41:42 -0400

    Changes for Post method

--- chatbot.py
+++ chatbot.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from models.database import db
 import os
+import json
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-#app.config['SQLALCHEMY_DATABASE_URI'] = 
-#db.init_app(app)
 
-@app.route('/' methods=['POST'])
+
+# app.config['SQLALCHEMY_DATABASE_URI'] =
+# db.init_app(app)
+
+@app.route('/', methods=['POST'])
 def hello_world():
-    response=
-        {
-        speech:'Hello World!'
-    }
-    return response
-
+    data = {}
+    data['speech'] = 'Hello World!'
+    json_data = json.dumps(data)
+    return json_data
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ['PORT']))
+    app.run(app.run(host='0.0.0.0', port=int(os.environ['PORT'])))
