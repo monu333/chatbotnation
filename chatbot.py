959a89dad35de343f45287bd2899eb5d07cf920d
bmthanki <bhargavthanky@gmail.com>
2017-10-28 16:22:46 -0400

    Revert "Changes to get facebook data"
    This reverts commit e93f710b770ab560914f9a496dab7bfa7321180c.

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask import make_response
 from models.database import db
 import os
 import json
@@ -13,24 +12,11 @@ app.config['DEBUG'] = True
 
 @app.route('/', methods=['POST'])
 def hello_world():
-    #data = {}
-    #data['displayText'] = 'Hello World!'
-    #data['speech']= 'Hello Hi World'
-    #json_data = json.dumps(data)
-    #return json_data
-    speech = "Hi Peter, How are you?"
-    res={
-        "speech": speech,
-        "displayText": speech,
-        "data": {"facebook": "Hi Peter"},
-        # "contextOut": [],
-        "source": "Test"
-    }
-    res = json.dumps(res, indent=4)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
+    data = {}
+    data['displayText'] = 'Hello World!'
+    data['speech']= 'Hello Hi World'
+    json_data = json.dumps(data)
+    return json_data
 
 if __name__ == '__main__':
-    app.run()
+    app.run(app.run(host='0.0.0.0', port=int(os.environ['PORT'])))
